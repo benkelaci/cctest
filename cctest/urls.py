012fe448238b19fc38e104a01f4bd6f62e3a7ac6
laszlo.benke <laszlo.benke@gravityrd.com>
2016-11-11 09:04:41 +0100

    tutorial 4

--- cctest/urls.py
+++ cctest/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    #url(r'^test/', include('mentor_candidates.urls')),
+    url(r'^test/', include('mentor_candidates.urls')),
     url(r'^admin/', admin.site.urls),
 ]
 
--- mentor_candidates/views.py
+++ mentor_candidates/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render, HttpResponse
 from .models import Mentor
 
-# def detail(request, mentor_id):
-#     return HttpResponse("You're looking at question %s." % mentor_id)
-#
-# def opinion(request, mentor_id):
-#     return HttpResponse("You're voting on question %s." % mentor_id)
+def detail(request, mentor_id):
+    mentor = get_object_or_404(Mentor, pk=mentor_id)
+    # try:
+    #     mentor = Mentor.objects.get(pk=mentor_id)
+    # except Mentor.DoesNotExist:
+    #     raise Http404("Mentor does not exist")
+    return render(request, 'mentor_candidates/detail.html', {'mentor': mentor})
+
+def opinion(request, opinion_id):
+    return HttpResponse("You're voting on opinion %s." % opinion_id)
+
+
+
+def index(request):
+    mentor_list = Mentor.objects.all()
+    # template = loader.get_template('mentor_candidates/index.html')
+    context = {
+        'mentor_list': mentor_list,
+    }
+    return render(request, 'mentor_candidates/index.html', context)
+    # return HttpResponse(template.render(context, request))
